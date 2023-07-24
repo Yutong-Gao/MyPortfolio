f40c930d87cf7642fe1233b3828252da1a1ad5f5
Yutong-Gao <gao.yuto@northeastern.edu>
2023-07-24 17:15:21 -0400

    aboutme component

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -38,10 +38,10 @@ export default function Header() {
 
   const getHeaderOptionsClass = (index) => {
     let classes = "header-option";
-    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator";
+    if (index < TOTAL_SCREENS.length - 1) classes += " header-option-seperator";
 
-    if (selectedScreen === index) classes += "selected-header-option";
-    return;
+    if (selectedScreen === index) classes += " selected-header-option";
+    return classes;
   };
 
   const switchScreen = (index, screen) => {
